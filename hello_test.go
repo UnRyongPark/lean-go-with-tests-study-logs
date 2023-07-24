78d984b8b84daa97bb562a53c1090254a7849840
unryongpark <theblack1025@gmail.com>
2023-07-24 23:00:48 +0900

    테스트 선 작성

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,9 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	// Hello + 이름으로 응답오게 테스트를 먼저 수정한다.
+	got := Hello("Brian")
+	want := "Hello, Brian"
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
