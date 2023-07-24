727e154ae65f46afb958d4259ba71fa08b742e17
unryongpark <theblack1025@gmail.com>
2023-07-24 23:07:26 +0900

    테스트 추가

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	// Hello + 이름으로 응답오게 테스트를 먼저 수정한다.
-	got := Hello("Brian")
-	want := "Hello, Brian"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		// Hello + 이름으로 응답오게 테스트를 먼저 수정한다.
+		got := Hello("Brian")
+		want := "Hello, Brian"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		// 이름이 없으면 Hello, World를 출력한다.
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
